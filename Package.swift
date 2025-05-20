3ee0bc1b7af040d599d91f95be778072c71bb927
Rokas Sidaravicius <rokas.sidaravicius@presentconnection.eu>
2025-05-20 16:56:18 +0300

    temp fix

--- Package.swift
+++ Package.swift
@@ -22,11 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "NFCPassportReader",
-            dependencies: ["OpenSSL"]),
+            name: "NFCPassportReader"),
         .testTarget(
             name: "NFCPassportReaderTests",
-            dependencies: ["NFCPassportReader", "OpenSSL"]),
+            dependencies: ["NFCPassportReader"]),
     ]
 )
 
